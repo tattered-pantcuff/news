b72cffa838153976a96904b38ef85a100c5e0596
christina <tatteredpantcuff@gmail.com>
2024-12-08 14:55:26 -0500

    custom user model

--- django_project/accounts/models.py
+++ django_project/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
+    # blank is validation related, null is database related
+    # if blank=False, then a form would require a value
